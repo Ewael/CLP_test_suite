494daeef42dc2078acfdaf2a40faec29f54a5e72
Turodoras <turodoras@gmail.com>
2020-06-23 16:35:07 +0200

    [MYCAT][ADD] possibility to cat without filename

--- mycat/with_stdio/mycat.c
+++ mycat/with_stdio/mycat.c
@@ -1,30 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 4096
+#define STDIN 0
+#define STDOUT 1
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc > 2)
     {
         printf("Usage: ./mycat filename\n");
         return EXIT_SUCCESS;
     }
 
-    FILE *fp;
-    fp = fopen(argv[1], "r");
-    if (fp == NULL)
+    if (argc == 1)
     {
-        perror("main: could not open file");
-        exit(EXIT_FAILURE);
+        char buffer[BUFFER_SIZE] = { 0 };
+        int r;
+        while (1)
+        {
+            r = read(STDIN, buffer, BUFFER_SIZE);
+
+            int w;
+            size_t offset = 0;
+            while (r > 0)
+            {
+                w = write(STDOUT, buffer + offset, r);
+                if (w == -1)
+                {
+                    perror("main: error when writing in stdout");
+                }
+                offset += w;
+                r -= w;
+            }
+        }
     }
 
-    char buffer[BUFFER_SIZE];
-    while (fgets(buffer, BUFFER_SIZE, fp) != NULL)
+    else if (argc == 2)
     {
-        printf(buffer);
-    }
+        FILE *fp;
+        fp = fopen(argv[1], "r");
+        if (fp == NULL)
+        {
+            perror("main: could not open file");
+            exit(EXIT_FAILURE);
+        }
 
-    fclose(fp);
-    return EXIT_SUCCESS;
+        char buffer[BUFFER_SIZE] = { 0 };
+        while (fgets(buffer, BUFFER_SIZE, fp) != NULL)
+        {
+            printf(buffer);
+        }
+
+        fclose(fp);
+        return EXIT_SUCCESS;
+    }
 }
