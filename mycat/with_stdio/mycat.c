5dadfaf4b5605405380101c2fd6ea70a35558726
Turodoras <turodoras@gmail.com>
2020-06-23 22:16:20 +0200

    [MYCAT][ADD] adjustements after review

--- mycat/with_stdio/mycat.c
+++ mycat/with_stdio/mycat.c
@@ -5,57 +5,16 @@
 
 #include "mycat.h"
 
-#define BUFFER_SIZE 4096
-#define STDIN 0
-#define STDOUT 1
-
-/*
- * read stdin and write it back in stdout
- */
-void cat_stdin(void)
-{
-    char buffer[BUFFER_SIZE] = { 0 };
-    int r;
-    while (1)
-    {
-        r = read(STDIN, buffer, BUFFER_SIZE);
-
-        int w;
-        size_t offset = 0;
-        while (r > 0)
-        {
-            w = write(STDOUT, buffer + offset, r);
-            if (w == -1)
-            {
-                perror("main: error when writing in stdout");
-                exit(EXIT_FAILURE);
-            }
-            offset += w;
-            r -= w;
-        }
-    }
-}
-
 /*
  * read file and write it in stdout
  */
-void cat_file(char *filename)
+void cat(FILE *fp)
 {
-    FILE *fp;
-    fp = fopen(filename, "r");
-    if (fp == NULL)
+    char buffer[CAT_BUFFER_SIZE] = { 0 };
+    while (fgets(buffer, CAT_BUFFER_SIZE, fp) != NULL)
     {
-        perror("cat_file: could not open file");
-        exit(EXIT_FAILURE);
+        printf("%s", buffer);
     }
-
-    char buffer[BUFFER_SIZE] = { 0 };
-    while (fgets(buffer, BUFFER_SIZE, fp) != NULL)
-    {
-        printf(buffer);
-    }
-
-    fclose(fp);
 }
 
 int main(int argc, char **argv)
@@ -63,7 +22,7 @@ int main(int argc, char **argv)
     // read from stdin
     if (argc == 1)
     {
-        cat_stdin();
+        cat(stdin);
     }
 
     // usage if -h or --help
@@ -75,9 +34,18 @@ int main(int argc, char **argv)
     // read specified file(s)
     else
     {
-        for (int i=1; i < argc; i++)
+        for (int i = 1; i < argc; i++)
         {
-            cat_file(argv[i]);
+            FILE *fp = fopen(argv[i], "r");
+            if (!fp)
+            {
+                perror("cat_file: could not open file");
+                exit(EXIT_FAILURE);
+            }
+
+            cat(fp);
+
+            fclose(fp);
         }
     }
 
